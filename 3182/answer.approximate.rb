967074e1d6213690513f419b3147e547b0677a72
CIARANA <kiwamura0314@gmail.com>
2017-06-16 14:28:05 +0900

    Update for refactoring

--- 3182/answer.approximate.rb
+++ 3182/answer.approximate.rb
@@ -5,4 +5,4 @@
 
 require "prime"
 f=gets.to_f
-p (1..(1/f-f/3).to_i).inject(0){|s,i|s+=((i*i)+1).prime_division.inject(1){|d,e|d*=e[1]+1}/2}
+p (1..(1/f-f/3).to_i).inject(0){|s,i|s+=(i*i+1).prime_division.inject(1){|d,e|d*=e[1]+1}/2}

74fef80f7472a69d22e9abe3ab41f1299c3c3471
CIARANA <kiwamura0314@gmail.com>
2017-06-16 14:25:48 +0900

    Add answer.approximate.rb for codeiq3182

---
+++ 3182/answer.approximate.rb
@@ -0,0 +1,8 @@
+# 1/tan(x) = cot(x) ~ 1/x-x/3
+# とすると
+# [0.464, 0.786, 0.787, 0.788, 0.789, 0.790, 0.791]
+# がうまくいかない
+
+require "prime"
+f=gets.to_f
+p (1..(1/f-f/3).to_i).inject(0){|s,i|s+=((i*i)+1).prime_division.inject(1){|d,e|d*=e[1]+1}/2}

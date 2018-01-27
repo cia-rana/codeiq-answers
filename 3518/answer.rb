abaf058fc50cbc74da419197c3cc108f07688358
CIARANA <kiwamura0314@gmail.com>
2018-01-28 00:02:40 +0900

    Update answer.rb

--- 3518/answer.rb
+++ 3518/answer.rb
@@ -1,9 +1,2 @@
 require 'prime'
-s=gets.to_r
-puts (if s.denominator == 1
-  s.to_i
-elsif s.denominator.prime_division.all?{|e,_|e==2||e==5}
-  s.to_f
-else
-  s
-end)
+puts (s=gets.to_r).denominator.prime_division.all?{|e,_|e==2||e==5}?"%g"%s.to_f: s
